29705dd00eb1b2bad401474be24e6044613e5087
Jaskoder <belgedia6@gmail.com>
2025-11-17 22:58:41 +0100

    Add input and children methods to Element class

--- scripts/core/Element.js
+++ scripts/core/Element.js
@@ -1,5 +1,6 @@
 import { isString } from "./helpers.js";
 import { isArray } from "./helpers.js";
+import { Factory } from "./Factory.js";
 
 export class Element {
     constructor(tag) {
@@ -35,6 +36,30 @@ export class Element {
     click( clickFunc ) {
         if(typeof clickFunc === 'function') {
             this.element.addEventListener('click', clickFunc)
+        }
+        return this;
+    }
+    input(inputFunc) {
+        if(typeof inputFunc === 'function') {
+            this.element.addEventListener('input', inputFunc)
+        }
+        return this
+    }
+    children( children ) {
+        if(Array.isArray(children)) {
+            children.forEach((child) => {
+                if(child instanceof HTMLElement) {
+                    this.element.appendChild(child);
+                } else if (child instanceof Element) {
+                    this.element.appendChild( Factory.getDomElement(child));
+                }
+            });
+        } else {
+            if(children instanceof HTMLElement) {
+                this.element.appendChild(children)
+            } else if (children instanceof HTMLElement) {
+                this.element.appendChild( Factory.getDomElement(children));
+            }
         }
         return this;
     }
